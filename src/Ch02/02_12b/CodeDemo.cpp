1c88ae49e23c0299e134815f31357e6992324e17
AEASH-cloud <54107648+AEASH-cloud@users.noreply.github.com>
2026-01-20 19:18:44 +0000

    practiced and conquered

--- src/Ch02/02_12b/CodeDemo.cpp
+++ src/Ch02/02_12b/CodeDemo.cpp
@@ -5,15 +5,25 @@
 #include <iostream>
 #include <cstdint>
 
-enum cow_purpose {dairy, meat, hide, pet};
+#define MEAT 8
+
+enum class cow_purpose {dairy, meat, hide, pet};
+enum class grocery_section {canned=10, frozen=22, meat=60, laundry=12, dairy=15, baker=45};
 
 int main(){
-    int a;
+    int meat = MEAT;
+    //a is an instance of cow_purpose?
+    cow_purpose b;
+    grocery_section c;
+    
+    //create a simple variable to call that stores the enum item? 
+    b = cow_purpose::hide;
+    c = grocery_section::baker;
 
-    a = meat;
-
-    std::cout << "a = " << a << std::endl;
-
+    std::cout<<"b = "<< (int) b << std::endl;
+    std::cout<<"meat price = "<< meat << std::endl;
+    
+    std::cout<<"baker price = "<< (int)c << std::endl;
     std::cout << std::endl << std::endl;
     return (0);
 }
