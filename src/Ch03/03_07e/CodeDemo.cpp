24c79059f9dfde3c193a8ad7531d47af82042905
AEASH-cloud <54107648+AEASH-cloud@users.noreply.github.com>
2026-01-21 04:14:54 +0000

    added notes

--- src/Ch03/03_07e/CodeDemo.cpp
+++ src/Ch03/03_07e/CodeDemo.cpp
@@ -7,13 +7,13 @@
 #include "cow.h"
 
 int main(){
-    cow *my_cow;
+    cow *my_cow;//this is where you create a pointer to an object by usingn *
 
-    my_cow = new cow("Gertie", 3, cow_purpose::hide);
-    std::cout << my_cow->get_name() << " is a type-" << (int) my_cow->get_purpose() << " cow." << std::endl;
+    my_cow = new cow("Gertie", 3, cow_purpose::hide);//had to add "new" in front of cow (indicating you're creating a new object)
+    std::cout << my_cow->get_name() << " is a type-" << (int) my_cow->get_purpose() << " cow." << std::endl;//had to replace "." with "->" between the object and its attribute you're callig
     std::cout << my_cow->get_name() << " is " << my_cow->get_age() << " years old." << std::endl;
     
-    delete my_cow;
+    delete my_cow;//BEST PART! you can delete the object right after running it, saving space in memory!!! 
     
     std::cout << std::endl << std::endl;
     return (0);
