faef77e47fc17c743f004649e79eb82851308fd5
AEASH-cloud <54107648+AEASH-cloud@users.noreply.github.com>
2026-01-22 15:17:54 +0000

    worked for loops

--- src/Ch04/04_04b/CodeDemo.cpp
+++ src/Ch04/04_04b/CodeDemo.cpp
@@ -7,8 +7,26 @@
 
 int main(){
     std::vector<int> numbers = {12, 25, 31, 47, 58};
+    
     float average;
     
+    average = 0.0f;
+    
+    //loops through the vector to add the numbers to average -- 
+    for(int i = 0; i<numbers.size(); i++)
+        average += numbers[i];//you're adding the current element to the average and accumulating the result 
+    average /= numbers.size();//actually getting the new average of numbers by dividing the new sum by the size of the list of numbers
+    std::cout << "Average: " << average << std::endl;
+
+    average = 0.0f;
+    for (auto x : numbers)//
+        average += x;//
+    average /= numbers.size();
+    std::cout << "Average: " << average << std::endl;
+
+    
+
+
     std::cout << std::endl << std::endl;
     return (0);
 }
