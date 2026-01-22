6fa0050fe15101f36c3e776baedc3a4dcbba0eaf
AEASH-cloud <54107648+AEASH-cloud@users.noreply.github.com>
2026-01-22 04:10:51 +0000

    works!

--- src/Ch04/04_03b/CodeDemo.cpp
+++ src/Ch04/04_03b/CodeDemo.cpp
@@ -8,8 +8,18 @@
 int main(){
     std::vector<int> numbers = {12, 25, 31, 47, 58};
     
-    std::vector<int>::iterator ptr = numbers.begin();
+    auto ptr = numbers.begin();
+
+    while (ptr != numbers.end()){//basically, keep the while loop going until it reaches the end of the vector 
+        ptr = next(ptr,1);//but have to manually tell the iterator to move forward one position
+    }
+    int i = 0;
     
+    do{
+        std::cout<<*ptr <<" ";
+        i++;//then must manually increment up
+    } while (i <numbers.size());//keep looping through as long as the iterator variable (number of times gone through the loop) is smaller than the size of the data structure, which we get by using the .size() function for vectors
+
     std::cout << std::endl << std::endl;
     return (0);
 }
